af0d8c1aee13871938a5b6e8df6c1e14b67f9762
Hishor <Hishor.ramanathan@gmail.com>
2021-12-30 15:16:58 +0100

    Update Pferd.java

--- Pferderennen/src/pferderennen/Pferd.java
+++ Pferderennen/src/pferderennen/Pferd.java
@@ -10,5 +10,17 @@ package pferderennen;
  * @author Hisho
  */
 public class Pferd {
-    
+
+    private String name;
+    private int startnummer;
+    private int gerannteStrecke = 0;
+
+    public Pferd(String name, int startnummer) {
+        this.name = name;
+        this.startnummer = startnummer;
+
+    }
+    public void rennen(){
+        
+    }
 }
