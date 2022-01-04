77f76636e4bd7247d88671021bcf38fc080a24fc
Hishor <Hishor.ramanathan@gmail.com>
2022-01-04 12:38:56 +0100

    Update Pferderennen.java

--- Pferderennen/src/pferderennen/Pferderennen.java
+++ Pferderennen/src/pferderennen/Pferderennen.java
@@ -12,40 +12,63 @@ import java.util.ArrayList;
  * @author Hisho
  */
 public class Pferderennen {
+    private  String rangliste;
     private int streckenLaenge;
     ArrayList<Pferd> Pferde = new ArrayList<>();
-    
+
     public Pferderennen(int streckenLaenge, int anzahlPferde) {
         this.streckenLaenge = streckenLaenge;
         for (int i = 0; i < anzahlPferde; i++) {
-            Pferde.add(new Pferd("Pferd "+i, i));
+            Pferde.add(new Pferd("Pferd " + i, i));
         }
     }
-    
-    private String gerannteStreckeAlsBalken(Pferd pferd){
-        
+
+    private String gerannteStreckeAlsBalken(Pferd pferd) {
+
         return "";
     }
-    
-    public void lassPferdeRennen(){
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
-    public void createPferde(){
+
+    public void lassPferdeRennen() {
+        int rang = 0;
+        while (rennenZuEnde()) {                       
         for (Pferd pferd : Pferde) {
-      
+            if(pferd.getGerannteStrecke()>=getStreckenLaenge()){
+               rang++;  
+               rangliste+= rang +". Rang: " + pferd.toString()+"\n";
+            }else{
+                pferd.rennen();
+            }
+        }
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            // Kein Problem
+        }
+        
         }
     }
+
+    public boolean rennenZuEnde() {
+        boolean rennenzuende = false;
+        for (Pferd pferd : Pferde) {
+            if(pferd.getGerannteStrecke()<getStreckenLaenge()){
+            rennenzuende= true;
+            break;
+            }
+        }
+        return rennenzuende ;
+    }
+
+    public String getStand(){
+        return "Aktuellerstand";
+    }
+
+    public String getRangliste() {
+        return rangliste;
+    }
+
+    public int getStreckenLaenge() {
+        return streckenLaenge;
+    }
+
 }
