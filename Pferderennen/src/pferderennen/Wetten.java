b391caaa64a956b1fd05fab18d6428992da4e578
Hishor <Hishor.ramanathan@gmail.com>
2022-02-17 16:11:41 +0100

    Update Wetten.java

--- Pferderennen/src/pferderennen/Wetten.java
+++ Pferderennen/src/pferderennen/Wetten.java
@@ -12,48 +12,56 @@ import java.util.List;
  *
  * @author Hisho
  */
-public class Wetten {   
-  
-   private Pferd pferd1;
-   private Pferd pferd2;
-   private Pferd pferd3;
-   private int einsatz;
-    public Wetten(int einsatz,Pferd pferd1, Pferd pferd2, Pferd pferd3) {
+public class Wetten {
+
+    private Pferd pferd1;
+    private Pferd pferd2;
+    private Pferd pferd3;
+    private int einsatz;
+
+    public Wetten(int einsatz, Pferd pferd1, Pferd pferd2, Pferd pferd3) {
         this.pferd1 = pferd1;
         this.pferd2 = pferd2;
         this.pferd3 = pferd3;
-        this.einsatz=einsatz;
+        this.einsatz = einsatz;
     }
-    
-    public Wetten(int einsatz,Pferd pferd1, Pferd pferd2) {
-         this.einsatz=einsatz;
+
+    public Wetten(int einsatz, Pferd pferd1, Pferd pferd2) {
+        this.einsatz = einsatz;
         this.pferd1 = pferd1;
         this.pferd2 = pferd2;
         this.pferd3 = null;
-     
+
     }
-     public Wetten(int einsatz,Pferd pferd1) {
-         this.einsatz=einsatz;
-         this.pferd1 = pferd1;
-         this.pferd2 = null;
+
+    public Wetten(int einsatz, Pferd pferd1) {
+        this.einsatz = einsatz;
+        this.pferd1 = pferd1;
+        this.pferd2 = null;
         this.pferd3 = null;
     }
-    
-    public int checkWette(ArrayList<Pferd> pferde){
-        if(pferd3!=null){
-             if(pferde.get(0).getName().equals(pferd1.getName())||pferd2.getName().equals(pferde.get(0).getName())||pferde.get(0).equals(pferd3.getName())){
-               return einsatz*4;
+
+    public int checkWette(ArrayList<Pferd> pferde) {
+        if (pferd3 != null) {
+            if (pferde.get(0).getName().equals(pferd1.getName()) || pferd2.getName().equals(pferde.get(0).getName()) || pferde.get(0).equals(pferd3.getName())) {
+                pferd1 = null;
+                pferd2 = null;
+                pferd3 = null;
+                return einsatz * 4;
             }
-        }else if(pferd2!=null){
-            if(pferde.get(0).getName().equals(pferd1.getName())||pferd2.getName().equals(pferde.get(0).getName())){
-               return einsatz*6;
-           }
-        }else if(pferd1!=null){
-           if(pferde.get(0).getName().equals(pferd1.getName())){
-               return einsatz*8;
-           }
-        }       
-            return -einsatz;
+        } else if (pferd2 != null) {
+            if (pferde.get(0).getName().equals(pferd1.getName()) || pferd2.getName().equals(pferde.get(0).getName())) {
+                pferd1 = null;
+                pferd2 = null;
+                return einsatz * 6;
+            }
+        } else if (pferd1 != null) {
+            if (pferde.get(0).getName().equals(pferd1.getName())) {
+                pferd1 = null;
+                return einsatz * 8;
+            }
+        }
+        return -einsatz;
     }
-    
+
 }
